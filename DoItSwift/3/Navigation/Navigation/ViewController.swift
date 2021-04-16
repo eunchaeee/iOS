6714b3012cd5f210f55cd4750bbcf3ee80a0640a
Ellie <ecjeong@maxst.com>
2021-04-16 10:59:19 +0900

    navigation view controller

--- DoItSwift/3/Navigation/Navigation/ViewController.swift
+++ DoItSwift/3/Navigation/Navigation/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editButton" {
+            // 버튼을 클릭한 경우
+            editViewController.textWayValue = "segue : use button"
+        } else if segue.identifier == "editBarButton" {
+            // 바 버튼을 클릭한 경우
+            editViewController.textWayValue = "segue : use Bar button"
+        }
+    }
 
 }
 
